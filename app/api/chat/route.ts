9cd2b03eb7e40775b18b9db9a7ade16b54b42f00
Reda <mahmoudreda457@gmail.com>
2026-02-05 10:54:00 +0200

    Chat with AI

---
+++ app/api/chat/route.ts
@@ -0,0 +1,29 @@
+import { huggingface } from "@ai-sdk/huggingface";
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+    const modelMessages = await convertToModelMessages(messages);
+
+    const result = streamText({
+      model: huggingface('deepseek-ai/DeepSeek-V3-0324'),
+      messages: modelMessages,
+    });
+
+    // log the usage of the stream
+    result.usage.then((usage) => {
+      console.log({
+        messageCount: messages.length,
+        inputTokens: usage.inputTokens,
+        outputTokens: usage.outputTokens,
+        totalTokens: usage.totalTokens,
+      });
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error(`Error streaming chat completion: ${error}`);
+    return Response.json({ error: 'Failed to stream chat completion' }, { status: 500 });
+  }
+}
